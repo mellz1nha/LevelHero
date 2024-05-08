4b931593431c580f3c1aa29f6a2029177042a5b0
Mell Romão <105497299+mellz1nha@users.noreply.github.com>
2024-05-08 18:38:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ let heroSexo = "M"
 console.log("Bem vindo" + nickname)
 console.log(nickname + "Entrou no servidor")
 
-let  XP for menor do que 1.000 = Ferro
-let  XP for entre 1.001 e 2.000 = Bronze
-let  XP for entre 2.001 e 5.000 = Prata
-let  XP for entre 5.001 e 7.000 = Ouro
-let  XP for entre 7.001 e 8.000 = Platina
-let  XP for entre 8.001 e 9.000 = Ascendente
-let  XP for entre 9.001 e 10.000= Imortal
-let  XP for maior ou igual a 10.001 = Radiante
+let  XP1 for menor do que 1.000 = Ferro
+let  XP2 for entre 1.001 e 2.000 = Bronze
+let  XP3 for entre 2.001 e 5.000 = Prata
+let  XP4 for entre 5.001 e 7.000 = Ouro
+let  XP5 for entre 7.001 e 8.000 = Platina
+let  XP6 for entre 8.001 e 9.000 = Ascendente
+let  XP7 for entre 9.001 e 10.000= Imortal
+let  XP8 for maior ou igual a 10.001 = Radiante
 
 let selecionavel = true
 
