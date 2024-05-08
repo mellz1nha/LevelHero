736758a54f5f8bb8fba7e812145be5ceeb0f7545
Mell Romão <105497299+mellz1nha@users.noreply.github.com>
2024-05-08 19:38:42 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ let timeHero= [
 
 console.log("Bem vindo" + nickname)
 console.log(nickname + "Entrou no servidor")
+console.log(timeHero[0][1])
 
-let  XP1 for menor do que 1.000=Ferro
-let  XP2 for entre 1.001 e 2.000=Bronze
-let  XP3 for entre 2.001 e 5.000 = Prata
-let  XP4 for entre 5.001 e 7.000 = Ouro
-let  XP5 for entre 7.001 e 8.000 = Platina
-let  XP6 for entre 8.001 e 9.000 = Ascendente
-let  XP7 for entre 9.001 e 10.000= Imortal
-let  XP8 for maior ou igual a 10.001 = Radiante
+let  XP1  menor do que 1.000=Ferro
+let  XP2  entre 1.001 e 2.000=Bronze
+let  XP3  entre 2.001 e 5.000 = Prata
+let  XP4  entre 5.001 e 7.000 = Ouro
+let  XP5  entre 7.001 e 8.000 = Platina
+let  XP6  entre 8.001 e 9.000 = Ascendente
+let  XP7  entre 9.001 e 10.000= Imortal
+let  XP8  maior ou igual a 10.001 = Radiante
 
 let selecionavel=true
 
