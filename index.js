bc71d2fea1f3e1ec9744a34c880441136c4b5b4a
Mell Romão <105497299+mellz1nha@users.noreply.github.com>
2024-05-08 18:59:04 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 console.log("Level Hero");
-let nickname = "Vítor"
-let heroSexo = "M"
-let Level = 1.000
+
+let nickname="vítor"
+let HeroSex="M"
+let Level="1.000"
+
 
 console.log("Bem vindo" + nickname)
 console.log(nickname + "Entrou no servidor")
 
-let  XP1 for menor do que 1.000 = Ferro
-let  XP2 for entre 1.001 e 2.000 = Bronze
+let  XP1 for menor do que 1.000=Ferro
+let  XP2 for entre 1.001 e 2.000="Bronze"
 let  XP3 for entre 2.001 e 5.000 = Prata
 let  XP4 for entre 5.001 e 7.000 = Ouro
 let  XP5 for entre 7.001 e 8.000 = Platina
@@ -15,8 +17,8 @@ let  XP6 for entre 8.001 e 9.000 = Ascendente
 let  XP7 for entre 9.001 e 10.000= Imortal
 let  XP8 for maior ou igual a 10.001 = Radiante
 
-let selecionavel = true
+let selecionavel=true
 
-const notificacao = "Level Hero diz: " 
+const notificacao="Level Hero diz: " 
 
-console.log (notificacao + "Você alcancou o nível")
+console.log(notificacao + "Você alcancou o nível")
