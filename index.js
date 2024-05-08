58e8d282682d52b98b5cbbf60034f99c979a3a20
Mell Romão <105497299+mellz1nha@users.noreply.github.com>
2024-05-08 18:14:31 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("Level Hero");
+let nickname = ["Vítor"]
+
+console.log("Bem vindo" + nickname)
+console.log(nickname + "Entrou no servidor")
+
+let Se XP for menor do que 1.000 = Ferro
+let Se XP for entre 1.001 e 2.000 = Bronze
+let Se XP for entre 2.001 e 5.000 = Prata
+let Se XP for entre 5.001 e 7.000 = Ouro
+let Se XP for entre 7.001 e 8.000 = Platina
+let Se XP for entre 8.001 e 9.000 = Ascendente
+let Se XP for entre 9.001 e 10.000= Imortal
+let Se XP for maior ou igual a 10.001 = Radiante
+
+const notificacao = "Level Hero diz: " 
+
+console.log (notificacao + "Você alcancou o nível")
