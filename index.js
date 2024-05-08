a9a9511e26ab2e3b7444d645e00a5e743d6e4bf8
Mell Romão <105497299+mellz1nha@users.noreply.github.com>
2024-05-08 19:33:05 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 console.log("Level Hero");
 
-let nickname="vítor"
-let HeroSex="M"
-let Level="1.000"
-
+let nickname = ["Vítor" , "Josy" , "Smith"]
+let HeroSex= ["M" , "F"]
+let Level="XP1"
+let timeHero= [
+    ["Vítor" , "M" , "XP5"]
+    ["Smith" , "F" , "XP8"]
+    ]
 
 console.log("Bem vindo" + nickname)
 console.log(nickname + "Entrou no servidor")
 
 let  XP1 for menor do que 1.000=Ferro
-let  XP2 for entre 1.001 e 2.000="Bronze"
+let  XP2 for entre 1.001 e 2.000=Bronze
 let  XP3 for entre 2.001 e 5.000 = Prata
 let  XP4 for entre 5.001 e 7.000 = Ouro
 let  XP5 for entre 7.001 e 8.000 = Platina
